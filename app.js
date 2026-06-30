3573600a85b4c875bddd0f8e9e28836ecccfded7
Hassan-Arain995 <forpubg061@gmail.com>
2026-06-30 16:41:47 +0500

    Update app.js

--- app.js
+++ app.js
@@ -293,13 +293,12 @@
     return div.innerHTML;
   }
 
-  // Service Worker Registration
+    // Service Worker Registration
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
+      navigator.serviceWorker.register('service-worker.js')
         .then(reg => console.log('SW registered:', reg.scope))
         .catch(err => console.error('SW registration failed:', err));
     });
   }
 })();
-    
