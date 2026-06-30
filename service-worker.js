2debb768a1c476dd6ebe5d1caa2bdd89a3ef6d4f
Hassan-Arain995 <forpubg061@gmail.com>
2026-06-30 16:29:20 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,8 +10,8 @@ const FILES_TO_CACHE = [
   '/anime.js',
   '/trash.js',
   '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', (e) => {
